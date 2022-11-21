21849ca0a29d4afb11b58d5d74eeb44d1c0fbaab
matheus <pg50649@alunos.uminho.pt>
2022-11-21 18:17:50 +0000

    Add Makefile and header dir

--- source/CPoint.h
+++ source/CPoint.h
@@ -16,11 +16,11 @@ public:
     CPoint(unsigned int x,unsigned int y);
     CPoint(SCoord c);
     ~CPoint();
-    float       get_distance(CPoint &p);
-    float       get_distance(SCoord &c);
-    SCoord      get_point();
-    bool        set_point(SCoord &c);
-    bool        set_point(CPoint &p);
+    float  get_distance(CPoint &p);
+    float  get_distance(SCoord &c);
+    SCoord get_point();
+    bool   set_point(SCoord &c);
+    bool   set_point(CPoint &p);
 };
 
 #endif /*__CPoint__*/
