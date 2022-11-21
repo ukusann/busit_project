741552e2e44b003f1ad3ebea3e44cd9262ca0bcc
matheus <pg50649@alunos.uminho.pt>
2022-11-21 18:02:08 +0000

    Add CNode tests

--- source/CNode.cpp
+++ source/CNode.cpp
@@ -3,12 +3,8 @@
 #define MASK_OPEN  128
 
 //=================Constructor========================
-CNode::CNode(uint16_t id, uint8_t byte_info, uint16_t gain = 0, SCoord c = {0,0}): CPoint(c) 
-{
-    this->id         = id;
-    this->byte_info  = byte_info;
-    this->gain       = gain;
-}
+CNode::CNode(uint16_t id, uint8_t byte_info, uint16_t gain = 0, SCoord c = {0,0})
+    : CPoint(c), id(id), byte_info(byte_info), gain(gain)  {}
 //====================================================
 
 //=================Destructor=========================
@@ -16,14 +12,14 @@ CNode::~CNode(){}
 //====================================================
 
 //=================Get Node Info======================
-uint8_t CNode::get_nodeInfo()
+uint8_t CNode::get_node_info()
 {
     return this->byte_info;
 }
 //====================================================
 
 //=================Set Node Info======================
-void CNode::set_nodeInfo(uint8_t byte_info)
+void CNode::set_node_info(uint8_t byte_info)
 {
     this->byte_info = byte_info;
 }
@@ -34,7 +30,7 @@ bool CNode::is_open()
 {
     bool flag = true;
 
-    if (MASK_OPEN & byte_info == 0)
+    if ((MASK_OPEN & byte_info) == 0)
         flag = false;
     
     return flag;
@@ -42,20 +38,20 @@ bool CNode::is_open()
 //====================================================
 
 //=================Get Direction======================
-uint8_t CNode::get_direction(){
-    return 0;
+float CNode::get_direction(CPoint &p){
+    return this->get_distance(p); 
 }
 //====================================================
 
 //=================Set Gain===========================
-void CNode::setGain(uint16_t G)
+void CNode::set_gain(uint16_t G)
 {
     this->gain = G;
 }
 //====================================================
 
 //=================Get Gain===========================
-uint16_t CNode::getGain()
+uint16_t CNode::get_gain()
 {
     return this->gain;
 }
--- source/main.cpp
+++ source/main.cpp
@@ -1,35 +1,88 @@
-#include "CPoint.h"
+//#include "CPoint.h"
+#include "CNode.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    //----------------Init variable--------------------
-    CPoint p(1,1);
-    CPoint p2(1,2);
-    SCoord c{1, 3};
-    //-------------------------------------------------a
+     //================CPoint test======================
+     //================================================= 
+     /*
+     //----------------Init variable--------------------
+     CPoint p(1,1);
+     CPoint p2(1,2);
+     SCoord c{1, 3};
+     //-------------------------------------------------
 
-    //----------------Distance test--------------------
-    cout << "distance: " << p.get_distance(p2) << endl;
-    cout << "distance: " << p.get_distance(c) << endl;
-    //-------------------------------------------------
+     //----------------Distance test--------------------
+     cout << "distance: " << p.get_distance(p2) << endl;
+     cout << "distance: " << p.get_distance(c) << endl;
+     //-------------------------------------------------
 
     //----------------Get point test--------------------
-    SCoord c2 = p.get_point();
-    cout << "initial point:\n" << "x = " << c2.x
-         << endl << "y = " << c2.y << endl;
-    //--------------------------------------------------
+     SCoord c2 = p.get_point();
+     cout << "initial point:\n" << "x = " << c2.x
+          << endl << "y = " << c2.y << endl;
+     //--------------------------------------------------
 
-    //----------------Set point test--------------------
-    p.set_point(c);
-    cout << "new point position:\n" << "x = " << p.get_point().x
-         << endl << "y = " << p.get_point().y << endl;
+     //----------------Set point test--------------------
+     p.set_point(c);
+     cout << "new point position:\n" << "x = " << p.get_point().x
+          << endl << "y = " << p.get_point().y << endl;
 
-    p.set_point(p2);
-    cout << "new point position:\n" << "x = " << p.get_point().x
-         << endl << "y = " << p.get_point().y << endl;
-    //--------------------------------------------------
+     p.set_point(p2);
+     cout << "new point position:\n" << "x = " << p.get_point().x
+          << endl << "y = " << p.get_point().y << endl;
+     //--------------------------------------------------
+     */
+     //================================================= 
+
+     //================CNode test=======================
+     //================================================= 
+     //----------------Init variable--------------------
+     CPoint p(2,2);
+     SCoord s{0, 0};
+     SCoord s2{2, 1};
+     CNode n(1, 4, 0, s);        // byte -> 0000 0100
+     CNode n2(2, 133, 0, s2);    // byte -> 1000 0101 
+     //-------------------------------------------------
+
+     //----------------Get Node Info--------------------
+     cout << "Node 1 Byte info: " << (unsigned)n.get_node_info() << endl;
+     cout << "Node 2 Byte info: " << (unsigned)n2.get_node_info() << endl;
+     //-------------------------------------------------
+
+     //----------------Set Node Info--------------------
+     n.set_node_info(5);
+     cout << "Node 1 new Byte info: " << (unsigned)n.get_node_info() << endl;
+     //-------------------------------------------------
+
+     //----------------Is Open--------------------------
+     if(n.is_open() == true)
+          cout << "Node 1 is open. " << endl;
+     else
+          cout << "Node 1 is not open. " << endl;
+
+     if(n2.is_open() == true)
+          cout << "Node 2 is open. " << endl;
+     else
+          cout << "Node 2 is not open. " << endl;
+     //-------------------------------------------------
+
+     //----------------Get Direction--------------------
+     cout << "Node 1 distance to goal: " << n.get_direction(p) << endl;
+     //-------------------------------------------------
+
+     //----------------Get Gain-------------------------
+     cout << "Node 2 current gain: " << n2.get_gain() << endl;
+     //-------------------------------------------------
+
+     //----------------Set Gain-------------------------
+     n2.set_gain(2);
+     cout << "Node 2 new gain: " << n2.get_gain() << endl;
+     //-------------------------------------------------
+     //================================================= 
+
     return 0;
 }
